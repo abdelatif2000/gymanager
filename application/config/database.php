d9259f1e3e409bf63eb41124fe71c2ce108ef42d
abdelatif_benichou <abdelatif.benichou@cbo.ma>
2023-01-04 00:26:05 +0100

    change the database properties

--- application/config/database.php
+++ application/config/database.php
@@ -35,9 +35,9 @@
 */
 
 // The following values will probably need to be changed.
-$db['default']['username'] = "root";
-$db['default']['password'] = "";
-$db['default']['database'] = "gymanager";
+$db['default']['username'] = "elassiouimohamed_gymanager";
+$db['default']['password'] = "+zd(8FkI[.%SlCsx~*";
+$db['default']['database'] = "elassiouimohamed_gymanager";
 
 // The following values can probably stay the same.
 $db['default']['hostname'] = "localhost";
